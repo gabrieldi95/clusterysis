5c67b07ace44cdb54b430a9885aa299b6d9e85c5
Gabriel Di Pardi Arruda <gabrielpardi@outlook.com>
2022-05-11 19:51:04 -0300

    first innit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='clusterysis',
+    packages=find_packages(include=['clusterysis']),
+    version='0.1.0',
+    description='A library for visualizing clusters.',
+    author='Gabriel Di Pardi Arruda',
+    license='MIT',
+    install_requires=['plotly', 'pandas']
+)
