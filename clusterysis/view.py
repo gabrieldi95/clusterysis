640506e258ad198aedc10563c6c034ff928aec9f
Gabriel Di Pardi Arruda <gabrielpardi@outlook.com>
2022-05-12 21:07:11 -0300

    added pie charts

--- clusterysis/view.py
+++ clusterysis/view.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 from IPython.display import display, HTML
 from plotly.subplots import make_subplots
@@ -5,7 +7,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 class View():
-    def __init__(self, df, cluster_col, quant_cols=None, cat_cols=None):
+    def __init__(self, df, cluster_col, quant_cols=None, cat_cols=None, diff_cluster=None):
         self.df = df
         self.quant_cols = quant_cols if quant_cols else _get_quant_cols(df, cluster_col)
         self.cat_cols = cat_cols if cat_cols else _get_cat_cols(df, cluster_col)
@@ -14,8 +16,40 @@ class View():
         self.cluster_col = cluster_col
         self.df_cluster = df[cluster_col]
         self.n_clusters = df[cluster_col].unique().shape[0]
+        self.diff_cluster = diff_cluster
 
         self.view_table()
+        self.view_pies()
+
+    def view_pies(self):
+        df_pie = self.df.copy()
+
+        for var in self.cat_cols:
+            if df_pie[var].apply(len).mean() < 15:
+                textinfo = 'percent+label'
+                textposition = None
+            else:
+                df_pie[var] = df_pie[var].apply(lambda x: f"{x[:25]}...")
+                textinfo = 'percent'
+                textposition = 'inside'
+
+            rows=2
+            cols=int(math.ceil(self.n_clusters/2))
+
+            fig = make_subplots(rows=rows, cols=cols, specs=[[{'type':'domain'} for i in range(cols)], [{'type':'domain'} for i in range(cols)]])
+
+            i = 0
+            for name, group_df in df_pie.groupby(self.cluster_col):
+                row = int(i//cols+1)
+                col = int(i%cols+1)
+                width = 2 if name==self.diff_cluster else None
+                fig.add_trace(go.Pie(labels=group_df[var], values=group_df[self.quant_cols[0]], name=name, title=f"Cluster {name}", hole=.4, marker={'line': {'width': width}}), row, col)
+                i+=1
+            fig.update_traces(hoverinfo="label+percent+name", textinfo=textinfo, textposition=textposition)
+            fig.update_layout(
+                title_text=f"{var} percentage in each cluster.",
+                height=1000)
+            fig.show()
 
     def view_table(self):
         grouped_df = self._group_by_cluster()
